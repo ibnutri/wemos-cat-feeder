de470db806b754d439a19131da0e63224115d60b
triyono, ibnu <ibnu.triyono@gmail.com>
2016-10-12 15:38:50 +0700

    add virtual pin to trigger servo to swipe 90 degree

--- wemos_cat_feeder/wemos_cat_feeder.ino
+++ wemos_cat_feeder/wemos_cat_feeder.ino
@@ -28,25 +28,43 @@
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <Servo.h>
 
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "YourAuthToken";
+char* auth = "";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "YourNetworkName";
-char pass[] = "YourPassword";
+char* ssid = "";
+char* pass = "";
+
 
 void setup()
 {
   config();
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
+  myservo.attach(D5);  // attaches the servo on pin 9 to the servo object
+  myservo.write(0); // starting position for servo
 }
 
 void loop()
 {
   Blynk.run();
+}
+BLYNK_WRITE(2){
+  if(param.asInt() == 1){
+    swipeServo();
+  }
+}
+void swipeServo(){
+  myservo.write(90);
+  Serial.println("servo 90");
+  delay(2000);
+  myservo.write(0);
+  Serial.println("servo back");
 }
 
